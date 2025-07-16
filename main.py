adf148743c7348945539ba99478c704a171c90c1
jammit <james.mitsumasu@gmail.com>
2025-07-15 22:59:26 -0500

    print file contents and support function to get text

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(filepath):
+
+  with open(filepath) as file:
+    file_contents = file.read()
+
+    return file_contents
+
+
+
+
+def main():
+  print(get_book_text("./books/frankenstein.txt"))
+
+
+
+
+main()
