7183af1391e19c9b8e983533260a55e9b16b3248
jammit <james.mitsumasu@gmail.com>
2025-07-17 00:05:18 -0500

    Added command line inputs and handling

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 from stats import word_count, character_count, sorted_dict
 
 def get_book_text(filepath):
@@ -10,26 +13,31 @@ def get_book_text(filepath):
 
 def main():
 
-  file_path = "./books/frankenstein.txt"
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
-  print("============ BOOKBOT ============")
-  print(f"Analyzing book found at {file_path}...")
-  print("----------- Word Count ----------")
-  num_words = word_count(get_book_text(file_path))
+  else:
+    file_path = sys.argv[1]
 
-  print(f"Found {num_words} total words")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    num_words = word_count(get_book_text(file_path))
 
-  num_characters = character_count(get_book_text(file_path))
+    print(f"Found {num_words} total words")
 
-  print("--------- Character Count -------")
-  sorted_list = sorted_dict(num_characters)
+    num_characters = character_count(get_book_text(file_path))
 
-  for item in sorted_list:
-    if item["char"].isalpha():
-      print(f"{item["char"]}: {item["num"]}")
+    print("--------- Character Count -------")
+    sorted_list = sorted_dict(num_characters)
+
+    for item in sorted_list:
+      if item["char"].isalpha():
+        print(f"{item["char"]}: {item["num"]}")
 
 
-  print("============= END ===============")
+    print("============= END ===============")
 
 
 
